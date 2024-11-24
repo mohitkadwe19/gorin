02f3272c805b6f38cd50a283e4c3986df306527c
mohitkadwe19 <mohitpawar530@gmail.com>
2024-11-24 19:38:35 +0530

    📦 NEW: add QuotesCard component for displaying quotes with themed styling and interactive elements

---
+++ components/QuotesCard.tsx
@@ -0,0 +1,59 @@
+import { StyleSheet, Image, Dimensions } from "react-native";
+
+import { ThemedView } from "./ThemedView";
+import { ThemedText } from "./ThemedText";
+
+export function QuotesCard() {
+  const width = Dimensions.get("window").width;
+
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedView style={styles.card}>
+        <Image
+          source={require("@/assets/images/icons/quotes.png")}
+          style={styles.icon}
+        />
+        <ThemedText style={styles.cardText}>
+          In that era, there will be neither famine or war, envy or competition,
+          for good will flow in abundance and all the delightswill be freely
+          available as dust. The occupation of the entire world will be solerly
+          to know G-d.
+        </ThemedText>
+        <ThemedText style={styles.cardBottomText}>- Mishneh Tarah</ThemedText>
+      </ThemedView>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    backgroundColor: "#EBF0F1",
+  },
+  icon: {
+    width: 36,
+    height: 36,
+    marginBottom: 10,
+  },
+  card: {
+    width: "90%",
+    padding: 20,
+    margin: 20,
+    borderRadius: 20,
+    backgroundColor: "#fff",
+  },
+  cardText: {
+    fontFamily: "Prata",
+    fontWeight: 400,
+    fontSize: 14,
+    lineHeight: 26,
+    color: "rgba(0, 0, 0, 0.7)",
+  },
+  cardBottomText: {
+    fontFamily: "Prata",
+    fontWeight: 400,
+    fontSize: 16,
+    lineHeight: 55,
+    color: "#000000",
+  },
+});
